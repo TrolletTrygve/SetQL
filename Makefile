e55c4884380cb2c881f75e9c9e0a27287f66a408
Sebastian Johansson <zazati99@gmail.com>
2022-09-30 15:13:47 +0200

    Initial make setup

---
+++ Makefile
@@ -0,0 +1,26 @@
+#_*_ Makefile _*_
+
+# System independent config
+OUTPUT 		= a
+
+INCLUDE_DIR = include
+SRC_DIR 	= src
+OUTPUT_DIR 	= out
+
+OBJS = main.o
+
+# System specific makefile
+ifeq ($(OS),Windows_NT)
+    detected_OS := Windows
+else
+    detected_OS := $(shell uname)
+endif
+
+
+ifeq ($(detected_OS), Windows)
+	include make/windows.mk
+endif
+
+ifeq ($(detected_OS), Linux)
+	include make/linux.mk
+endif
---
+++ make/windows.mk
@@ -0,0 +1,37 @@
+CC = gcc
+
+
+INCLUDE = -I$(INCLUDE_DIR)
+DBFLAGS = -O2 -fstrict-aliasing
+
+WFLAGS 		= 	-Wall -Wmissing-include-dirs -Wswitch-default -Wswitch-enum  	\
+				-Wunused-parameter -Wextra -Werror -Wfatal-errors  				\
+				-Wmissing-prototypes -Wstrict-prototypes -Wmissing-declarations \
+				-Wshadow -Wlarger-than-1048576 -Wconversion						\
+				-Wfloat-equal -Wlong-long -Wredundant-decls -Wbad-function-cast	\
+				-Wcast-qual -Wcast-align -Wwrite-strings -Wpointer-arith  		\
+				-Winline -Wdisabled-optimization 								\
+				-Wold-style-definition -Wmissing-noreturn -Wuninitialized		\
+				-Winit-self -Winvalid-pch -Wpacked -Wpadded -Wunreachable-code	
+
+WNOFLAGS	=	-Wno-conversion -Wno-unused-result
+
+CXXFLAGS 	= $(INCLUDE) $(DBFLAGS) $(WFLAGS) $(WNOFLAGS)
+
+
+all: init target
+
+
+init:
+	- mkdir $(BUILD_DIR) $(OUTPUT_DIR) $(INCLUDE_DIR) 
+
+clean:
+	- del $(OBJS) $(OUTPUT_DIR)\$(OUTPUT)
+
+
+target: $(OBJS)
+	$(CC) $^ -o $(OUTPUT_DIR)\$(OUTPUT)
+
+
+%.o: $(SRC_DIR)\%.c
+	$(CC) "$<" $(CXXFLAGS) -c -o $@
