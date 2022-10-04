5e3d2e6a3f2a7ac2c12da55d6689084f48ec3266
Ruiz968 <jose.ruiz.alarcon@gmail.com>
2022-10-04 15:57:47 +0200

    Code added to parser

--- src/parser.c
+++ src/parser.c
@@ -1,14 +1,133 @@
 #include "parser.h"
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+
+static char** copy_string_array(const char* strings[], size_t length) {
+    char** copy = (char**)malloc(length * sizeof(char*));
+    for (size_t i = 0; i < length; i++) {
+        copy[i] = (char*)malloc(strlen(strings[i]) * sizeof(char));
+        strcpy(copy[i], strings[i]);
+    }
+    return copy;
+}
+
+static void modify_string_list(string_list* str_list, const char** strings, size_t length) {
+    char** copy = copy_string_array(strings, length);
+    str_list->length = length;
+    str_list->strings = copy;
+}
+
+static char*** copy_string_matrix2d(const char** values[], size_t length_1, size_t lenght_2) {
+    char*** copy = (char***)malloc(length_1 * sizeof(char**));
+    for(size_t i = 0; i < length_1; i++) {
+        copy[i] = copy_string_array(values[i], lenght_2);
+    }
+    return copy;
+}
+
+static void modify_array_list(array_list* a_list, const char** values[], size_t length, size_t data_types_length) {
+    char*** copy = copy_string_matrix2d(values, length, data_types_length);
+    a_list->length = length;
+    a_list->values = copy;
+}
 
 universe create_universe_example(void){     // TODO
     universe u;
+    // Set universe name
 	strcpy(u.name, "Animal");
+
+    // Set key and attributes names and types
+    const char* key_data_type_names[] = {"STRING"};
+    const char* key_data_names[] = {"scientific_name"}; 
+    const char* attribute_data_type_names[] = {"STRING", "INTEGER"};
+    const char* attribute_data_names[] = {"name", "population"}; 
+    modify_string_list(&u.key_data_type_names, key_data_type_names, 1);
+    modify_string_list(&u.key_data_names, key_data_names, 1);
+    modify_string_list(&u.attribute_data_type_names, attribute_data_type_names, 2);
+    modify_string_list(&u.attribute_data_names, attribute_data_names, 2);
+
+    // Set universe keys
+    const char* key_value_1[] = {"\"Gallus gallus domesticus\""};
+    const char* key_value_2[] = {"\"Chiroptera\""};
+    const char* key_value_3[] = {"\"Corvus\""};
+    const char** key_values[] = {key_value_1, key_value_2, key_value_3};
+    modify_array_list(&u.key_values, key_values, 3, u.key_data_type_names.length);
+
+    // Set universe attributes
+    const char* attribute_value_1[] = {"\"Chicken\"", "25000000000"};
+    const char* attribute_value_2[] = {"\"Bat\"", "3000000000"};
+    const char* attribute_value_3[] = {"\"Crow\"", "100000"};
+    const char** attribute_values[] = {attribute_value_1, attribute_value_2, attribute_value_3};
+    modify_array_list(&u.attribute_values, attribute_values, 3, u.attribute_data_type_names.length);
+
+    //const char** attributes[] = {{'"crow"', '100000'}, {'"chicken"', '25000000000'}, {'"bat"', '3000000000'}};
+
     return u;
 }
 
+static void print_array_list(array_list a_list, size_t data_types_length) {
+    printf("[");
+    for (size_t i = 0; i < a_list.length; i++) {
+        char** strings = a_list.values[i];
+        printf("[");
+        for (size_t j = 0; j < data_types_length; j++) {
+            char* str = strings[j];
+            printf("%s", str);
+            if (j < data_types_length-1)
+                printf(", ");
+        }
+        printf("]");
+        if (i < a_list.length-1)
+            printf(", ");
+    }
+    printf("]");
+}
+
+static void print_key_values(universe u) {
+    print_array_list(u.key_values, u.key_data_type_names.length);
+}
+
+static void print_attribute_values(universe u) {
+    print_array_list(u.attribute_values, u.attribute_data_type_names.length);
+}
+
+void print_string_list(string_list strings){
+    printf("[");
+    for (size_t i = 0; i < strings.length - 1; i++) {
+        char* str = strings.strings[i];
+        printf("\"%s\", ", str);
+    }
+    if (strings.length > 0) {
+        char* str = strings.strings[strings.length-1];
+        printf("\"%s\"", str);
+    }
+    printf("]");
+}
+
 void print_universe(universe u){            // TODO
-    printf("Universe name: %s\n", u.name);
+    printf("Universe name: %s", u.name);
+    printf("\n\n");
+
+    printf("Key data type names: ");
+    print_string_list(u.key_data_type_names);
+    printf("\n");
+    printf("Key data names: ");
+    print_string_list(u.key_data_names);
+    printf("\n");
+    printf("Attribute data type names: ");
+    print_string_list(u.attribute_data_type_names);
+    printf("\n");
+    printf("Attribute data names: ");
+    print_string_list(u.attribute_data_names);
+    printf("\n\n");
+
+    printf("Key values: ");
+    print_key_values(u);
+    printf("\n\n");
+
+    printf("Attribute values: ");
+    print_attribute_values(u);
+    printf("\n\n");
 }
--- src/parser.h
+++ src/parser.h
@@ -12,9 +12,8 @@ typedef struct {
 } string_list;
 
 typedef struct {
-    string_list data_type_names;
-    void *values;
-    size_t length;
+    char*** values;                 // E.g. [['crow', 'chicken', 'bat'], [100000, 25000000000, 3000000000]]
+    size_t length;                  // E.g. 3
 } array_list;
 
 typedef struct {
@@ -25,6 +24,10 @@ typedef struct {
 typedef struct {
     char name[50];
     set *sets;
+    string_list key_data_type_names;        // E.g. {['STRING'], 1}
+    string_list key_data_names;             // E.g. {['scientific_name'], 1}
+    string_list attribute_data_type_names;  // E.g. {['STRING', 'INTEGER'], 2}
+    string_list attribute_data_names;       // E.g. {['name', 'population'], 2}
     array_list key_values;
     array_list attribute_values;
 } universe;
@@ -32,5 +35,7 @@ typedef struct {
 universe create_universe_example(void); // TODO
 
 void print_universe(universe u);    // TODO
+
+void print_string_list(string_list strings);
 
 #endif
