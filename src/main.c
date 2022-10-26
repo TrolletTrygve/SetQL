35913a9e1c965f16393f172b2de9c4d67a5448eb
Sebastian Johansson <sebbejohansson099@gmail.com>
2022-10-26 09:19:35 +0200

    meme

--- src/main.c
+++ src/main.c
@@ -1,12 +1,14 @@
-#include "parser.h"
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/wait.h>
 #include <string.h>
+
+#include "utils.h"
+#include "parser.h"
 #include "dbms_networking.h"
 #include "datastructures/database.h"
-#include "utils.h"
 #include "datastructures/bitset.h"
+#include "parser_db_translation.h"
 
 #define PIPE_READ_END 	0
 #define PIPE_WRITE_END 	1
@@ -17,17 +19,10 @@ int test_function(char msg[MAX_MESSAGE_SIZE], int size, client_id id)
 	printf("Message recieved: \"%s\" from %d with size %d\n", msg, id, size);
 
 
-	char memes[][64] = {"Hello", "my", "name", "is", "bla"}; 
+	
 
-	for (int i = 0; i < 5; i++)
-	{
-		printf("%s\n", memes[i]);
-	}
-
-	uint64_t meme_size = 5 * 64;
-
-	dbms_networking_send((char*)(&meme_size), 8, id);
-	dbms_networking_send((char*)(memes), meme_size, id);
+	//dbms_networking_send((char*)(&meme_size), 8, id);
+	//dbms_networking_send((char*)(memes), meme_size, id);
 
 	return 1;
 }
