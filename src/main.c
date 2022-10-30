33c874556f19dcf7518f578f878c4623c6a386ef
Sebastian Johansson <sebbejohansson099@gmail.com>
2022-10-30 12:07:14 +0100

    small fixes

--- src/main.c
+++ src/main.c
@@ -14,10 +14,18 @@
 #define PIPE_WRITE_END 	1
 
 static Database* db;
-static universe u;
 
-int test_function(char msg[MAX_MESSAGE_SIZE], int size, client_id id);
-int test_function(char msg[MAX_MESSAGE_SIZE], int size, client_id id)
+/**
+ * @brief      Callback function when the network 
+ *	
+ * @param[in]  msg   The recieved message 
+ * @param[in]  size  The size of the message
+ * @param[in]  id    The identifier of the client who sent the message
+ *
+ * @return     1 on success, 0 on fail
+ */
+int message_callback_func(char msg[MAX_MESSAGE_SIZE], int size, client_id id);
+int message_callback_func(char msg[MAX_MESSAGE_SIZE], int size, client_id id)
 {
 	printf("Message recieved: \"%s\" from %d with size %d\n", msg, id, size);
 
@@ -39,10 +47,8 @@ int test_function(char msg[MAX_MESSAGE_SIZE], int size, client_id id)
 
 		printf("Datalength: %ld, Column count: %ld\n", data_length, col_count);
 
-		
 		for (uint64_t i = 0; i < data_length; i++)
 		{
-			printf("%ld\n", i);
 			for (uint64_t j = 0; j < col_count; j++)
 			{
 				if (ret->columns[j].isString)
@@ -64,36 +70,41 @@ int test_function(char msg[MAX_MESSAGE_SIZE], int size, client_id id)
 
 	// Send amount of columns
 	dbms_networking_send((char*)(&ret->columnCount), 8, id);
-	// 
+
 	// Send length of data
 	dbms_networking_send((char*)(&ret->dataLength), 8, id);
-	// 
-	// For each column
-	// 
+
 	for (uint64_t i = 0; i < col_count; i++)
 	{
 		dbms_networking_send((char*)(&ret->columns[i].isString), 8, id);
+
 		uint64_t c_size = ret->columns[i].memorySize;
 		dbms_networking_send((char*)(&c_size), 8, id);
+
 		dbms_networking_send((char*)(ret->columns[i].data), c_size, id);
 	}
-	// 	send type for column
-	// 	send data size of column
-	//	send data for column
-
 
 
 	DEBUG_CALL(printf("DONE!\n");)
 	free_query(&q);
-
-	//dbms_networking_send((char*)(&meme_size), 8, id);
-	//dbms_networking_send((char*)(memes), meme_size, id);
-
 	return 1;
 }
 
-int main(void)
+int main(int argc, char** argv)
 {
+	// Check argument count
+	if (argc < 2)
+	{
+		fprintf(stderr, "Error: not enough arguments\n");
+		return -1;
+	}
+
+	if (argc > 2)
+	{
+		fprintf(stderr, "Error: too many arguments were given\n");
+		return -1;
+	}
+
 
 	// pipe
 	int fd[2];
@@ -110,12 +121,12 @@ int main(void)
 		return -1;
 	}
 
-	// Child process (terminal input)
+	// Child process (start server and initialize database)
 	if (pid == 0)
 	{	
 
-		u = create_empty_universe();
-		int error = parse_initialization(&u, "./example_db/language_example_clean.txt");
+		universe u = create_empty_universe();
+		int error = parse_initialization(&u, argv[1]);
 		if (error)
 		{
 			fprintf(stderr, "%s\n", "Error, parse_initialization");
@@ -127,6 +138,7 @@ int main(void)
 	 	db_addParsedData(db, &u);
 	 	free_universe(&u);
 
+	 	/*
 	 	query q = create_empty_query();
 	 	error = parse_query(&q, "SELECT name, population FROM inSweden;");
 	 	if (error)
@@ -149,15 +161,16 @@ int main(void)
 	 	}
 
 	 	free_query(&q);
+	 	*/
 
 		printf("DONE INITIALIZING\n");
-		dbms_networking_initialize(8080, &test_function);
+		dbms_networking_initialize(8080, &message_callback_func);
 		dbms_networking_add_pipe_client(fd[PIPE_READ_END], fd[PIPE_WRITE_END]);
 
 		dbms_start();
 		dbms_networking_kill();
 	}
-	// Parent process (start server)
+	// Parent process (terminal input)
 	else
 	{
 		printf("Parent! pid: %d\n", pid);
